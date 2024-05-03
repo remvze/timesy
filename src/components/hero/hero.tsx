e06a21739f7f51d2c0de925b8495e776db5bed9d
MAZE <remvze@proton.me>
2024-05-03 19:50:48 +0330

    feat: add logo to hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -6,7 +6,17 @@ export function Hero() {
   return (
     <div className={styles.hero}>
       <Container>
-        <h1 className={styles.title}>Timesy</h1>
+        <div className={styles.titleContainer}>
+          <img
+            alt="Timesy Logo"
+            className={styles.logo}
+            height={35}
+            src="/logo.svg"
+            width={30.31}
+          />
+
+          <h1 className={styles.title}>Timesy</h1>
+        </div>
         <p className={styles.desc}>A distraction-free online timer.</p>
       </Container>
     </div>
