160cbabff9f91ad15977eadee1800d479036b9fa
MAZE <remvze@proton.me>
2024-05-07 19:47:48 +0330

    feat: add fixed size to image

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -6,7 +6,13 @@ export function Hero() {
   return (
     <div className={styles.hero}>
       <Container>
-        <img alt="Timesy Logo" className={styles.logo} src="/logo.svg" />
+        <img
+          alt="Timesy Logo"
+          className={styles.logo}
+          height={45}
+          src="/logo.svg"
+          width={38.97}
+        />
 
         <h1 className={styles.title}>Timesy</h1>
         <p className={styles.desc}>A distraction-free online timer.</p>
