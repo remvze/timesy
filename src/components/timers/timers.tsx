a124fd6b4f1eac3e03072857851b8a77695dbbfc
MAZE <remvze@proton.me>
2024-05-21 14:03:43 +0430

    feat: add auto animation

--- src/components/timers/timers.tsx
+++ src/components/timers/timers.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 import { Timer } from './timer';
 import { Notice } from './notice';
@@ -8,6 +9,9 @@ import { useTimers } from '@/stores/timers';
 import styles from './timers.module.css';
 
 export function Timers() {
+  const [animationParent] = useAutoAnimate();
+  const [animationList] = useAutoAnimate();
+
   const timers = useTimers(state => state.timers);
   const spent = useTimers(state => state.spent());
   const total = useTimers(state => state.total());
@@ -15,23 +19,28 @@ export function Timers() {
   const spentMinutes = useMemo(() => Math.floor(spent / 60), [spent]);
   const totalMinutes = useMemo(() => Math.floor(total / 60), [total]);
 
-  return timers.length > 0 ? (
-    <div className={styles.timers}>
-      <header>
-        <h2 className={styles.title}>Timers</h2>
-        <div className={styles.line} />
-        {totalMinutes > 0 && (
-          <p className={styles.spent}>
-            {spentMinutes} / {totalMinutes} Minute{totalMinutes !== 1 && 's'}
-          </p>
-        )}
-      </header>
+  return (
+    <div ref={animationParent}>
+      {timers.length > 0 ? (
+        <div className={styles.timers} ref={animationList}>
+          <header>
+            <h2 className={styles.title}>Timers</h2>
+            <div className={styles.line} />
+            {totalMinutes > 0 && (
+              <p className={styles.spent}>
+                {spentMinutes} / {totalMinutes} Minute
+                {totalMinutes !== 1 && 's'}
+              </p>
+            )}
+          </header>
 
-      {timers.map(timer => (
-        <Timer id={timer.id} key={timer.id} />
-      ))}
+          {timers.map(timer => (
+            <Timer id={timer.id} key={timer.id} />
+          ))}
 
-      <Notice />
+          <Notice />
+        </div>
+      ) : null}
     </div>
-  ) : null;
+  );
 }
