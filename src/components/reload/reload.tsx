2b263e22538d2181ff818648bc46fafcc6be41bf
MAZE <remvze@proton.me>
2024-06-21 19:10:56 +0430

    fix: render reload modal only in the browser

--- src/components/reload/reload.tsx
+++ src/components/reload/reload.tsx
@@ -1,36 +1,11 @@
-import { useRegisterSW } from 'virtual:pwa-register/react'; // eslint-disable-line
+import { useEffect, useState } from 'react';
 
-import { Modal } from '@/components/modal';
-
-import styles from './reload.module.css';
+import { ReloadModal } from './reload-modal';
 
 export function Reload() {
-  const {
-    needRefresh: [needRefresh, setNeedRefresh],
-    updateServiceWorker,
-  } = useRegisterSW();
+  const [isBrowser, setIsBrowser] = useState(false);
 
-  const close = () => {
-    setNeedRefresh(false);
-  };
+  useEffect(() => setIsBrowser(true), []);
 
-  return (
-    <Modal show={needRefresh} onClose={close}>
-      <h2 className={styles.title}>New Content</h2>
-      <p className={styles.desc}>
-        New content available, click on reload button to update.
-      </p>
-
-      <div className={styles.buttons}>
-        <button onClick={close}>Close</button>
-
-        <button
-          className={styles.primary}
-          onClick={() => updateServiceWorker(true)}
-        >
-          Reload
-        </button>
-      </div>
-    </Modal>
-  );
+  return isBrowser ? <ReloadModal /> : null;
 }
