536d6b671f6c5aa172cc1746297e5dd31446794c
MAZE <remvze@proton.me>
2024-09-17 13:55:23 +0430

    feat: add volume settings

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,12 +1,20 @@
 import { useState, useMemo } from 'react';
+import { IoMdSettings } from 'react-icons/io';
 
+import { Modal } from '../modal';
 import { Field } from './field';
 
 import { useTimers } from '@/stores/timers';
 
 import styles from './form.module.css';
+import { useSettings } from '@/stores/settings';
 
 export function Form() {
+  const [showSettings, setShowSettings] = useState(false);
+
+  const volume = useSettings(state => state.volume);
+  const setVolume = useSettings(state => state.setVolume);
+
   const [name, setName] = useState('');
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(10);
@@ -35,76 +43,106 @@ export function Form() {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <Field
-        label="Timer Name"
-        optional
-        type="text"
-        value={name}
-        onChange={value => setName(value as string)}
-      />
-
-      <div className={styles.timeFields}>
+    <>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Field
-          label="Hours"
-          type="select"
-          value={hours}
-          onChange={value => setHours(value as number)}
-        >
-          {Array(13)
-            .fill(null)
-            .map((_, index) => (
-              <option key={`hour-${index}`} value={index}>
-                {index}
-              </option>
-            ))}
-        </Field>
+          label="Timer Name"
+          optional
+          type="text"
+          value={name}
+          onChange={value => setName(value as string)}
+        />
 
-        <Field
-          label="Minutes"
-          type="select"
-          value={minutes}
-          onChange={value => setMinutes(value as number)}
-        >
-          {Array(60)
-            .fill(null)
-            .map((_, index) => (
-              <option key={`minutes-${index}`} value={index}>
-                {index}
-              </option>
-            ))}
-        </Field>
+        <div className={styles.timeFields}>
+          <Field
+            label="Hours"
+            type="select"
+            value={hours}
+            onChange={value => setHours(value as number)}
+          >
+            {Array(13)
+              .fill(null)
+              .map((_, index) => (
+                <option key={`hour-${index}`} value={index}>
+                  {index}
+                </option>
+              ))}
+          </Field>
 
-        <Field
-          label="Seconds"
-          type="select"
-          value={seconds}
-          onChange={value => setSeconds(value as number)}
-        >
-          {Array(60)
-            .fill(null)
-            .map((_, index) => (
-              <option key={`seconds-${index}`} value={index}>
-                {index}
-              </option>
-            ))}
-        </Field>
-      </div>
+          <Field
+            label="Minutes"
+            type="select"
+            value={minutes}
+            onChange={value => setMinutes(value as number)}
+          >
+            {Array(60)
+              .fill(null)
+              .map((_, index) => (
+                <option key={`minutes-${index}`} value={index}>
+                  {index}
+                </option>
+              ))}
+          </Field>
 
-      <div className={styles.autoStart}>
-        <label>
-          <input
-            checked={autoStart}
-            type="checkbox"
-            onChange={() => setAutoStart(prev => !prev)}
-          />
-          Auto start the timer.
-        </label>
-      </div>
+          <Field
+            label="Seconds"
+            type="select"
+            value={seconds}
+            onChange={value => setSeconds(value as number)}
+          >
+            {Array(60)
+              .fill(null)
+              .map((_, index) => (
+                <option key={`seconds-${index}`} value={index}>
+                  {index}
+                </option>
+              ))}
+          </Field>
+        </div>
 
-      <button className={styles.button} type="submit">
-        Add Timer
-      </button>
-    </form>
+        <div className={styles.autoStart}>
+          <label>
+            <input
+              checked={autoStart}
+              type="checkbox"
+              onChange={() => setAutoStart(prev => !prev)}
+            />
+            Auto start the timer.
+          </label>
+        </div>
+
+        <div className={styles.buttons}>
+          <button className={styles.primary} type="submit">
+            Add Timer
+          </button>
+          <button type="button" onClick={() => setShowSettings(true)}>
+            <IoMdSettings />
+          </button>
+        </div>
+      </form>
+
+      <Modal show={showSettings} onClose={() => setShowSettings(false)}>
+        <div className={styles.settings}>
+          <h2>Settings</h2>
+
+          <div className={styles.field}>
+            <label htmlFor="volume">Volume</label>
+            <input
+              max={1}
+              min={0}
+              step={0.1}
+              type="range"
+              value={volume}
+              onChange={e => setVolume(+e.target.value)}
+            />
+          </div>
+
+          <div className={styles.notice}>
+            <strong>Notice:</strong> Changes to these settings will affect all
+            timers and are saved automatically.
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 }
