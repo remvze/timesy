11e5a0b9490536d9b2d6408a6aa28a54ed58a4e6
MAZE <remvze@proton.me>
2024-10-19 12:29:33 +0330

    feat: change hero

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,9 +1,15 @@
+import { cn } from '@/helpers/styles';
 import styles from './container.module.css';
 
 interface ContainerProps {
   children: React.ReactNode;
+  fullWidth?: boolean;
 }
 
-export function Container({ children }: ContainerProps) {
-  return <div className={styles.container}>{children}</div>;
+export function Container({ children, fullWidth }: ContainerProps) {
+  return (
+    <div className={cn(styles.container, fullWidth && styles.fullWidth)}>
+      {children}
+    </div>
+  );
 }
