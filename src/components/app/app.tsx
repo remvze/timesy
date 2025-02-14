8708aea51e5d2b52e33d53948fd390a06b45805b
MAZE <remvze@proton.me>
2025-02-14 22:02:10 +0330

    feat: add tabs

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,52 @@
+import { useState } from 'react';
+
 import { Container } from '@/components/container';
-// import { Form } from '@/components/form';
-// import { Timers } from '@/components/timers';
+import { Form } from '@/components/form';
+import { Timers } from '@/components/timers';
 import { StoreConsumer } from '@/components/store-consumer';
 import { SnackbarProvider } from '@/contexts/snackbar';
 import { PomodoroTimer } from '../pomodoro';
 
+import styles from './app.module.css';
+import { cn } from '@/helpers/styles';
+
 export function App() {
+  const [tab, setTab] = useState('countdown');
+
   return (
     <SnackbarProvider>
       <StoreConsumer>
         <Container>
-          {/* <Form /> */}
-          {/* <Timers /> */}
-          <PomodoroTimer />
+          <div className={styles.tabs}>
+            <button
+              className={cn(
+                styles.countdown,
+                tab === 'countdown' && styles.active,
+              )}
+              onClick={() => setTab('countdown')}
+            >
+              Countdown
+            </button>
+            <div className={styles.divider} />
+            <button
+              className={cn(
+                styles.pomodoro,
+                tab === 'pomodoro' && styles.active,
+              )}
+              onClick={() => setTab('pomodoro')}
+            >
+              Pomodoro
+            </button>
+          </div>
+
+          {tab === 'countdown' && (
+            <>
+              <Form />
+              <Timers />
+            </>
+          )}
+
+          {tab === 'pomodoro' && <PomodoroTimer />}
         </Container>
       </StoreConsumer>
     </SnackbarProvider>
